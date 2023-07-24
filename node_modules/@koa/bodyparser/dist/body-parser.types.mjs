e1a56447f32087f1dcdd3d1ff25017ccb234cf49
akira-cn <akira.cn@gmail.com>
2023-07-24 09:52:43 +0800

    v0.5.0

--- node_modules/@koa/bodyparser/dist/body-parser.types.mjs
+++
@@ -1,5 +0,0 @@
-// src/body-parser.types.ts
-var supportedBodyTypes = ["json", "form", "text", "xml"];
-export {
-  supportedBodyTypes
-};
--- node_modules/@koa/bodyparser/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { bodyParserWrapper as bodyParser, bodyParserWrapper as default } from './body-parser.js';
-import 'koa';
-import './body-parser.types.js';
-import 'co-body';
